e45c229533daca0b8cc956fa357b306c5600770d
ahhridoy01 <devilex.hridoy@gmail.com>
2025-08-25 02:12:34 +0600

    cashout function

--- home1.js
+++ home1.js
@@ -1,5 +1,6 @@
-const validPin=1234
-const ac="12345678910"
+// add money function
+const validPin = 1234
+const ac = "12345678910"
 document
     .getElementById("add-money-btn")
     .addEventListener("click", function () {
@@ -14,7 +15,7 @@ document
             document.getElementById("available-balance").innerText
         );
 
-        if (ac!==accountNumber) {
+        if (ac !== accountNumber) {
             alert("Invalid account Number");
             return;
         }
@@ -28,3 +29,32 @@ document
         document.getElementById("available-balance").innerText =
             totalNewAvailableBalance;
     });
+
+// toggling
+
+document.getElementById("add-button").addEventListener("click", function () {
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("add-money-parent").style.display = "block";
+});
+document
+    .getElementById("withdraw-btn")
+    .addEventListener("click", function () {
+        document.getElementById("add-money-parent").style.display = "none";
+        document.getElementById("cash-out-parent").style.display = "block";
+    });
+
+//  cash out function   
+
+document.getElementById("cash-out-btn").addEventListener("click", function () {
+
+    const amount = parseInt(document.getElementById("withdraw-amount").value);
+
+    const availableBalance = parseInt(
+        document.getElementById("available-balance").innerText
+    );
+
+    const totalNewAvailableBalance = availableBalance - amount;
+
+    document.getElementById("available-balance").innerText =
+        totalNewAvailableBalance;
+});
